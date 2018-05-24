bf85723a324669b7ff50b85b368c07b5daeba998
Jessica Son <jessicason18@gmail.com>
2018-05-23 19:05:34 -0700

    add

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,26 +11,41 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
   start();
 });
 
-// function which prompts the user for what action they should take
-function start() {
-  inquirer
-    .prompt({
-      name: "postOrBid",
-      type: "rawlist",
-      message: "Would you like to [POST] an auction or [BID] on an auction?",
-      choices: ["POST", "BID"]
-    })
-    .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.postOrBid.toUpperCase() === "POST") {
-        postAuction();
-      }
-      else {
-        bidAuction();
-      }
-    });
-}
+
+  function postAuction() {
+    inquirer
+      .prompt([
+        {
+          name: "item",
+          type: "input",
+          message: "What is the id of the item you would like to purchase?"
+        },
+        {
+          name: "units",
+          type: "input",
+          message: "How many units would you like to purchase?"
+        },
+      ])
+      .then(function(answer) {
+        connection.query(
+          "INSERT INTO bamazon SET ?",
+          {
+            
+          },
+          function(err) {
+            if (err) throw err;
+            console.log("Your auction was created successfully!");
+            // re-prompt the user for if they want to bid or post
+            start();
+          }
+        );
+      });
+  }
